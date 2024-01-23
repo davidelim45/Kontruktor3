4bed83ca543c1f2bcd7233d3bcbef74c9c6d688e
david.elim45 <davidelim170204@gmail.com>
2024-01-23 17:57:13 +0100

    k

---
+++ src/novas/Petta.java
@@ -0,0 +1,24 @@
+package novas;
+
+public class Petta {
+
+		    public static void main(String[] args) {
+		        int a = 5; // Внесете го вредноста на a
+		        int b = 10; // Внесете го вредноста на b
+		        int c = 3; // Внесете го вредноста на c
+		        int k;
+		        int d;
+
+		        if (a <= 5 && b > (a - c)) {
+		            k = 1;
+		            System.out.println("k = 1");
+		        } else {
+		            d = 0;
+		            System.out.println("d = 0");
+		        }
+		    }
+		
+
+	}
+
+
